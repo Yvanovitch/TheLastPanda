2f3f4fe802bca93748e1c1c6c7b6cb8891bee8d8
geekymoose <massonco@iro.umontreal.ca>
2017-01-21 02:12:49 -0500

    Create follow camera script + small refactoring

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    // -------------------------------------------------------------------------
+    // Variables
+    // -------------------------------------------------------------------------
+    public GameObject       target;
+    public float            distance;
+    public float            cameraSmoothing;
+
+
+    // -------------------------------------------------------------------------
+    // Unity functions - Override
+    // -------------------------------------------------------------------------
+    void Update () {
+        //Rotate camera to face elt
+        transform.rotation = target.transform.rotation;
+        //Move camera position.
+        Vector3 offset = target.transform.forward.normalized * distance;
+        Vector3 targetPos = target.transform.position - (target.transform.forward+offset);
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPos,
+            cameraSmoothing*Time.deltaTime);
+    }
+}
