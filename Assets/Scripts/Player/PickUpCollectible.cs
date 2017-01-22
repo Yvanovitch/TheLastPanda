b6f55cc1838ed7b7c71fe72a8098d726db152e80
geekymoose <massonco@iro.umontreal.ca>
2017-01-22 15:02:15 -0500

    Create pickup collectable

---
+++ Assets/Scripts/Player/PickUpCollectible.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCollectible : MonoBehaviour {
+    private int count = 0;
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Collectible") {
+            Debug.Log("Pick Up collectible");
+            other.transform.position = Vector3.zero;
+            count++;
+            
+            if(count == 10) {
+                Application.LoadLevel("WinScreen");
+            }
+        }
+    }
+}
