cdec19c8ec2655441a223f7dc5e9f6b71a33dc1a
geekymoose <massonco@iro.umontreal.ca>
2017-01-22 02:15:41 -0500

    Add growing and dying wave script + Update wave prefabs

--- Assets/Scripts/Wave/WaveMovement.cs
+++ Assets/Scripts/Wave/WaveMovement.cs
@@ -10,36 +10,42 @@ public class WaveMovement : MonoBehaviour {
     public float        minScale;
     public float        maxScale;
     public float        yStartPosition;
+    public float        growthSpeed;
+    public float        dyingSpeed;
+    public float        minLifeTime;
+    public float        maxLifeTime;
     public GameObject   wavePivot;
     public GameObject   playerObject;
+    public GameObject   waveManager;
+    public AudioSource  wavePopAudio;
     private float       speed;
-    private float       scale;
+    private float       scaleMaxRatio;
+    private Vector3     scaleMax;
+    private bool        isGrowing;
+    private bool        isDying;
+    private float       lifetime;
+    private float       timeBeforeDIE;
 
 
     // -------------------------------------------------------------------------
     // Unity functions - Override
     // -------------------------------------------------------------------------
     void Start() {
-        //Get random values
-        speed = Random.Range(minSpeed, maxSpeed);
-        scale = Random.Range(minScale, maxScale);
-        //Set wave
-        Vector3 playerPos = new Vector3(playerObject.transform.position.x, 0, playerObject.transform.position.z);
-        Vector3 wavePos = -playerPos;
-        //Debug.DrawRay(wavePivot.transform.position, playerPos, Color.red, 5f);
-        //Debug.DrawRay(wavePivot.transform.position, wavePos, Color.blue, 5f);
-        transform.localScale = new Vector3(
-            transform.localScale.x,
-            scale,
-            transform.localScale.z);
-        wavePivot.transform.LookAt(wavePos);
-        //Shift init y position to be at the ring bottom
-        float startPos = yStartPosition + GetComponent<Renderer>().bounds.size.y /2 ;
-        transform.Translate(new Vector3(0, startPos, 0)); //Shift Y pos to be at bottom
+        this.wavePop();
     }
 
     void Update() {
         wavePivot.transform.Rotate(new Vector3(0, speed, 0));
+        if(isGrowing) {
+            transform.localScale = Vector3.Lerp(transform.localScale, scaleMax, Time.deltaTime*growthSpeed);
+        }
+        else if(isDying) {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0f, scaleMax.y, 0f), Time.deltaTime*dyingSpeed);
+        }
+        timeBeforeDIE -= Time.deltaTime;
+        if(timeBeforeDIE<=0) {
+            waveDie();
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -52,4 +58,37 @@ public class WaveMovement : MonoBehaviour {
     // -------------------------------------------------------------------------
     // GamePlay functions
     // -------------------------------------------------------------------------
+    private void wavePop() {
+        //Get random values
+        speed           = Random.Range(minSpeed, maxSpeed);
+        scaleMaxRatio   = Random.Range(minScale, maxScale);
+        lifetime        = Random.Range(minLifeTime, maxLifeTime);
+        //Set wave
+        Vector3 playerPos = new Vector3(playerObject.transform.position.x, 0, playerObject.transform.position.z);
+        Vector3 wavePos = -playerPos;
+        wavePivot.transform.LookAt(wavePos);
+        //Debug.DrawRay(wavePivot.transform.position, playerPos, Color.red, 5f);
+        //Debug.DrawRay(wavePivot.transform.position, wavePos, Color.blue, 5f);
+        scaleMax = new Vector3(
+            transform.localScale.x,
+            scaleMaxRatio,
+            transform.localScale.z);
+        transform.localScale = scaleMax; //Will be reset to 0 but just used for bound size here
+        //Shift init y position to be at the ring bottom
+        float startPos = yStartPosition + GetComponent<Renderer>().bounds.size.y /2;
+        transform.Translate(new Vector3(0, startPos, 0)); //Shift Y pos to be at bottom
+
+        //Call start wave pop
+        transform.localScale = new Vector3(0f, scaleMax.y, 0f);
+        isGrowing       = true;
+        isDying         = false;
+        timeBeforeDIE   = lifetime;
+        wavePopAudio.Play();
+    }
+
+    private void waveDie() {
+        isGrowing = false;
+        isDying = true;
+        Destroy(gameObject, dyingSpeed); //Destroy in x seconds
+    }
 }
