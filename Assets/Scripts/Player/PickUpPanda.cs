b20d9edafe785a0fd230c099e6e9b066f8767d27
geekymoose <constantin.masson@umontreal.ca>
2017-01-21 23:20:07 -0500

    Add PickUp, Wave and PlayerHealth scripts

---
+++ Assets/Scripts/Player/PickUpPanda.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPanda : MonoBehaviour {
+    // -------------------------------------------------------------------------
+    // Variables
+    // -------------------------------------------------------------------------
+    public AudioSource  pickUpAudio;
+    private int         pickUpCounter;
+
+
+    // -------------------------------------------------------------------------
+    // Unity functions - Override
+    // -------------------------------------------------------------------------
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "PickUpPanda") {
+            pickUpCounter++;
+            pickUpAudio.Play();
+            Destroy(other);
+        }
+    }
+}
