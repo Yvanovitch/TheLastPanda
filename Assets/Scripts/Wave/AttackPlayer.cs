f02141a55c7a67f63533c13c9aef44416de9229b
geekymoose <constantin.masson@umontreal.ca>
2017-01-22 13:51:54 -0500

    Add AttackPlayer script that allow wave to harm player (Not added in Wave prefabs yet)

---
+++ Assets/Scripts/Wave/AttackPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour {
+    public float timeBeforeLooseScreen;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            Debug.Log("Player just Die hahahahaa! ");
+            PlayerHealth ph = other.GetComponent<PlayerHealth>();
+            ph.die();
+            Invoke("hahaYouLoose", timeBeforeLooseScreen); //Load loose scene in x sec
+        }
+    }
+
+    private void hahaYouLoose() {
+        Application.LoadLevel("LostScreen");
+    }
+}
